da0b073b27d1efbca474bd68ece2dac68d4db6aa
Menacho <Arlissonmenacho@gmail.com>
2021-05-13 13:36:23 -0400

    Primeiro Comit vscode

---
+++ Projeto/index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+
+const app = express()
+
+app.listen(3000,()=>console.log('servidor rodando porta 3000'))
+
+app.get('/',(req,res)=>res.send('Servidor rodando. Ok'))
+
+app.get('/Atendimentos',(req,res)=>res.send('Você está na rota de atendimentos'))
